41bcbc397840ce8aa6337a98e659c77631de15ae
Riaz Ahamed <railsdevriaz@gmail.com>
2018-06-09 09:58:29 +0000

    Add alerts for form submission, whitelist forms and add validation for the forms

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,20 @@ class ContactsController < ApplicationController
   end
   
   def create 
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = "Details Submitted sucessfully"
+      redirect_to new_contact_path
+    else
+      flash[:danger] = "Please check and try again!"
+      redirect_to new_contact_path
+    end
     
   end
+  
+  private
+    def contact_params
+      params.required(:contact).permit(:name, :email, :comments)
+    end
 end
