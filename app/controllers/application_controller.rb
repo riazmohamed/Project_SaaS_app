384f26e9d6af937ccde891f468ceee198fba7f23
Riaz Ahamed <railsdevriaz@gmail.com>
2018-06-17 06:11:30 +0000

    minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :stripe_card_token, :email, :password, :password_confirmation])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :stripe_card_token, :email, :password, :password_confirmation])
+    end
 end
